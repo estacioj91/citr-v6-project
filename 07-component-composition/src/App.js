5ae89a600feadc47c7f2ccddab1acbc4d6b8a300
John Estacio <john.estacio@workato.com>
2022-03-21 12:41:27 -0700

    working routers

--- 07-component-composition/src/App.js
+++ 07-component-composition/src/App.js
@@ -1,11 +1,25 @@
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Details from './Details'
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to ="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details></Details>
+          </Route>
+          <Route path="/">
+            <SearchParams></SearchParams>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
